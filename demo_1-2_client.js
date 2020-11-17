fd9c66721b73fd9148dadadf779565c4060665b8
Jocevičius, Karolis <karolis.jocevicius@seb.se>
2020-11-17 13:16:40 +0200

    change parenthesis

--- demo_1-2_client.js
+++ demo_1-2_client.js
@@ -1,21 +1,21 @@
 
 const http = require('http');
 
-console.time("requestCompute");
-console.time("requestHello");
+console.time('requestCompute');
+console.time('requestHello');
 
 console.log('Compute!');
 http.get('http://localhost:8000/compute', (resp) => {
-    console.timeEnd("requestCompute");
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
+    console.timeEnd('requestCompute');
+}).on('error', (err) => {
+    console.log('Error: ' + err.message);
 });
 
 setTimeout(() => {
     console.log('Hello!');
     http.get('http://localhost:8000/hello', (resp) => {
-        console.timeEnd("requestHello");
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
+        console.timeEnd('requestHello');
+    }).on('error', (err) => {
+        console.log('Error: ' + err.message);
     });
 }, 500)
