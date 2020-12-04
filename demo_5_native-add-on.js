7565cd8dfd0da22ac867dc86fe6cc6b56ed8f83f
Jocevičius, Karolis <karolis.jocevicius@seb.se>
2020-12-04 11:45:51 +0200

    some descriptions

--- demo_5_native-add-on.js
+++ demo_5_native-add-on.js
@@ -1,3 +1,9 @@
+/*
+    gyp - generate your project
+    Run `node-gyp configure` in ./native-addon folder to setup project
+    Run `node-gyp build` to build it
+*/
+
 const nativeModule = require('./native-addon/build/Release/greet.node');
 
 console.log(nativeModule);
